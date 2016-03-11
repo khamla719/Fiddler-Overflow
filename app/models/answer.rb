6943c138aac162d8407514242a901821400f129d
Amaar Fazlani & Khamla P. & Thomas Huang <pair+amfazlani.khamla719.tmashuang@devbootcamp.com>
2016-03-10 21:27:40 -0800

    Associations tested and works, added comments to have votes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,6 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   has_many :comments, as: :commentable
-
-
+  has_many :votes, as: :votable
   # Remember to create a migration!
 end
